d26e7f311bbdbca887696508f5a9c2b452bd8bfb
Joseph Salatore <14897294+Joe-J-Salvatore@users.noreply.github.com>
2017-11-16 17:00:27 -0800

    Added JS logic to DOM for MR, M+, and M- button clicks

--- Basic-Calculator/script2.js
+++ Basic-Calculator/script2.js
@@ -5,13 +5,14 @@ var key = '';
 var begRegex = /^[\+*\/0.]/g;
 var endRegex = /[\+*\/\-\(.]$/g;
 var ans = '';
+var memoryRecall = '';
 var reset = false;
-// eval() to get answers
+// DONE eval() to get answers
 // store answers in memory
 // add/subtract to and/or from memory
 // recall current memory
-// clear the display
-// disallow concatenating digits after equals has been clicked and answer displayed
+// DONE clear the display
+// DONE disallow concatenating digits after equals has been clicked and answer displayed
 // convert digit to percent, square root, change sign, power on
 // check for and clean display string for operators at beginning or end of string
 // ^[\+*\/0.] ... [\+*\/\-\(.]$
@@ -20,10 +21,27 @@ for (var i = 0; i < buttons.length; i += 1) {
             if (this.value === 'CLR') {
                 display.value = '';
             } else if (this.value === 'equals') {
+                memoryRecall = display.value[display.value.length - 1];
                 display.value = eval(display.value);
                 ans = display.value;
+                console.log('Memory: ' + memoryRecall);
+            } else if (this.value === 'MR') {
+                if (memoryRecall === '') {
+                    display.value = '0';
+                } else {
+                    display.value = eval(memoryRecall);
+                    ans = display.value;;
+                }
+            } else if (this.value === 'M+' || this.value === 'M-') {
+                if ('M+') {
+                    memoryRecall += '+' + display.value;
+                    console.log('Memory plus: ' + memoryRecall);
+                } else {
+                    memoryRecall -= '-' + display.value;
+                    console.log('Memory minus: ' + memoryRecall);
+                }
             } else {
-                if (ans !== '') {
+                if (ans !== '' && display.value !== '') {
                     display.value = this.value;
                     ans = '';
                 } else {
